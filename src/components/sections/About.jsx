cf61318ab864a29d965af9b7b7a6c2f08060e242
Rizwan <ahamadrizwan@320.com>
2025-02-28 21:05:59 +0530

    completed my portfolio project

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,110 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const About = () => {
+  const frontendSkills = [
+    "React",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "BootStrap",
+    "TailwindCSS",
+  ];
+
+  const stateManagement = ["React Context API", "Redux", "Redux Toolkit"];
+  const Tools = ["Git", "Github", "Linux", "Postman", "VSCode"];
+
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
+
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Passionate frontend developer with expertise in building scalable
+              web applications and creating innovative solutions.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> State Management</h3>
+                <div className="flex flex-wrap gap-2">
+                  {stateManagement.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Tools</h3>
+                <div className="flex flex-wrap gap-2">
+                  {Tools.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> Btech in Electronics and Communication </strong> -
+                  Maharishi University (Pursuing)
+                </li>
+                <li>
+                  <strong> Diploma in Electronics and Communication </strong> -
+                  Rameshwaram Institute of Technology and Management (2019 -
+                  2022)
+                  <p>Percentage: 86%</p>
+                </li>
+                <li>
+                  <strong> Intermediate </strong> - Tilottama Caampus (2017 -
+                  2019)
+                  <p>Percentage: 65%</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,246 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">
+                Myntra Clone - React & Redux Toolkit
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Developed a Myntra-like e-commerce frontend using React and
+                Redux Toolkit with product listing, filtering, and cart
+                features. Implemented React Router for navigation and structured
+                components for responsiveness.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Redux-Toolkit", "React-Router", "BootStrap"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/rizwan-droid/React-by-Prashant-sir/tree/main/Projects/9-myntra-clone"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Code →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">
+                Social Media App - React
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Built a social media app with Context API, useReducer, useState,
+                and React Router for navigation. Focused on maintainable
+                component structure and a seamless user experience.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {[
+                  "Context API",
+                  "Hooks(useReducer, useState, useRef, useEffect)",
+                  "React-Router",
+                  "BootStrap",
+                ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/rizwan-droid/React-by-Prashant-sir/tree/main/react-router"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Code →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">
+                Pig Game (Two-Player Dice Game) - JavaScript
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Built a two-player dice game with point accumulation, hold/reset
+                options, and dynamic updates via DOM manipulation, focusing on
+                smooth logic and user experience.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["JavaScript", "DOM Manipulation", "HTML", "CSS"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/rizwan-droid/Pig-Game"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Code →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">
+                Portfolio Website - React
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Developed a personal portfolio website using React to showcase
+                projects, skills, and experience. Integrated EmailJS for contact
+                form functionality, allowing seamless email communication.
+                Designed a responsive and modern UI for an engaging user
+                experience.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "TailwindCSS", "EmailJS", "Responsive"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/rizwan-droid/Pig-Game"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Code →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">
+                Tailoring Management System (Admin Panel) - React
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Developing an admin panel for a tailoring management system
+                using React, Redux Toolkit, and React Router. Focused on dynamic
+                UI for order creation and management.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {[
+                  "React",
+                  "React-Router",
+                  "Redux-Toolkit",
+                  "Dynamic UI with JSON (Testing Phase)",
+                ].map((tech) => (
+                  <span
+                    key={tech}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/rizwan-droid/adminPanel"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Code →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
